bd8715c6b244502ce4951f52c03d944285df4d6d
saiheroforking <notlasaikanna09@gmail.com>
2026-01-15 11:09:05 +0530

    deploy file changed

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^ubdre998ha#a50(^j7!jlg)9w(*-*8e=l6t*f$z%_1y3cne=d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+
 
 AUTH_USER_MODEL = 'blog.RegisterForm'
 LOGIN_REDIRECT_URL = 'post-list'
